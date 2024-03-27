f5d6e007a54ce259d1b08b4707782a7b2884f6d8
vvdam <maaalouladam@gmail.com>
2024-03-27 16:43:03 +0100

    s

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -21,5 +21,10 @@ function PokemonCard(pokemonprops) {
         </div>
     );
 }
-
+PokemonCard.propTypes = {
+    props:PropTypes.shape({
+        name:PropTypes.string.isRequired,
+        imgSrc:PropTypes.string,
+    }).isRequired,
+}
 export default PokemonCard;
